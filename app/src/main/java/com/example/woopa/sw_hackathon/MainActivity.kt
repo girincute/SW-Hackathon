82a581d691fd339650b20ee9e2e4983c07f65141
Woopa01 <whdnwls49@naver.com>
2018-08-31 02:02:00 +0900

    add new child xml

--- app/src/main/java/com/example/woopa/sw_hackathon/MainActivity.kt
+++ app/src/main/java/com/example/woopa/sw_hackathon/MainActivity.kt
@@ -2,11 +2,16 @@ package com.example.woopa.sw_hackathon
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
 
-class MainActivity : AppCompatActivity() {
-
+class MainActivity : AppCompatActivity(),OnMapReadyCallback {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    override fun onMapReady(p0: GoogleMap?) {
+    }
+
 }
